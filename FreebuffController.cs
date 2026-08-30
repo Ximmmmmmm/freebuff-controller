2690e7bd89249820573ef4fec10d3fb4246c53c9
Ximmmmmmm <124350858+Ximmmmmmm@users.noreply.github.com>
2026-08-30 21:49:18 +0800

    AssemblyVersion 1.0.0 → 1.3.0（对齐既有 v1.2.0 发布线，今日新增代理/汉化包功能为 minor 级）

--- FreebuffController.cs
+++ FreebuffController.cs
@@ -20,8 +20,8 @@ using System.Threading;
 using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
-[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersion("1.3.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.3.0.0")]
 
 namespace FreebuffController
 {
